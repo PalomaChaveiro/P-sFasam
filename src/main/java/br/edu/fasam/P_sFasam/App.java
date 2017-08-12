9acf72861f5256ead81ef1ea74e63064db0ef6e9
PalomaChaveiro <paloma_rodrigues@live.com>
2017-08-12 13:59:31 -0300

    Teste Commit

---
+++ src/main/java/br/edu/fasam/P_sFasam/App.java
@@ -0,0 +1,13 @@
+package br.edu.fasam.P_sFasam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
